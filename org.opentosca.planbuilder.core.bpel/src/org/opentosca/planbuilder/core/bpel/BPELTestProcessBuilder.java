420de8850e8eb71133ab0cc399f0d1a89b7dcd02
matejetz <simon.mat@t-online.de>
2018-02-27 19:45:34 +0100

    added first functionality of constructing empty test plans

--- org.opentosca.planbuilder.core.bpel/src/org/opentosca/planbuilder/core/bpel/BPELTestProcessBuilder.java
+++ org.opentosca.planbuilder.core.bpel/src/org/opentosca/planbuilder/core/bpel/BPELTestProcessBuilder.java
@@ -1,5 +1,6 @@
 package org.opentosca.planbuilder.core.bpel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.namespace.QName;
@@ -7,12 +8,26 @@ import javax.xml.namespace.QName;
 import org.opentosca.planbuilder.AbstractTestPlanBuilder;
 import org.opentosca.planbuilder.model.plan.AbstractPlan;
 import org.opentosca.planbuilder.model.tosca.AbstractDefinitions;
+import org.opentosca.planbuilder.model.tosca.AbstractNodeTemplate;
+import org.opentosca.planbuilder.model.tosca.AbstractServiceTemplate;
 
 public class BPELTestProcessBuilder extends AbstractTestPlanBuilder {
 
+    List<AbstractNodeTemplate> nodeTemplatesWithTest = new ArrayList<>();
+
+    public BPELTestProcessBuilder() {
+        // TODO Auto-generated constructor stub
+    }
+
     @Override
     public AbstractPlan buildPlan(final String csarName, final AbstractDefinitions definitions, final QName serviceTemplateId) {
-        // TODO Implement
+        final List<AbstractServiceTemplate> serviceTemplates = definitions.getServiceTemplates();
+        for(final AbstractServiceTemplate serviceTemplate : serviceTemplates) {
+            String namespace = serviceTemplate.getTargetNamespace();
+            if(namespace == null) {
+                namespace = definitions.getTargetNamespace();
+            }
+        }
         return null;
     }
 
--- org.opentosca.planbuilder/src/org/opentosca/planbuilder/AbstractTestPlanBuilder.java
+++ org.opentosca.planbuilder/src/org/opentosca/planbuilder/AbstractTestPlanBuilder.java
@@ -1,8 +1,77 @@
 package org.opentosca.planbuilder;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public abstract class AbstractTestPlanBuilder extends AbstractPlanBuilder{
+import org.opentosca.planbuilder.model.plan.ANodeTemplateActivity;
+import org.opentosca.planbuilder.model.plan.AbstractActivity;
+import org.opentosca.planbuilder.model.plan.AbstractPlan;
+import org.opentosca.planbuilder.model.plan.AbstractPlan.Link;
+import org.opentosca.planbuilder.model.tosca.AbstractDefinitions;
+import org.opentosca.planbuilder.model.tosca.AbstractNodeTemplate;
+import org.opentosca.planbuilder.model.tosca.AbstractPolicy;
+import org.opentosca.planbuilder.model.tosca.AbstractServiceTemplate;
+import org.opentosca.planbuilder.model.tosca.AbstractTopologyTemplate;
+import org.w3c.dom.Node;
 
-	//TODO: Implement if needed
+public abstract class AbstractTestPlanBuilder extends AbstractPlanBuilder {
 
+    private static final String OPEN_TOSCA_TEST_NAMESPACE = "http://opentosca.org/policytypes/annotations/tests";
+
+    /**
+     * Generates an ordered graph of the nodes that are to be tested
+     *
+     * @param id
+     * @param definitions
+     * @param serviceTemplate
+     * @return
+     */
+    protected AbstractPlan generateTestOG(final String id, final AbstractDefinitions definitions,
+                                          final AbstractServiceTemplate serviceTemplate) {
+        final Collection<AbstractActivity> activities = new ArrayList<>();
+        final Set<Link> links = new HashSet<>();
+
+        final Map<AbstractNodeTemplate, AbstractActivity> node2activityMap = new HashMap<>();
+
+        final AbstractTopologyTemplate topology = serviceTemplate.getTopologyTemplate();
+
+        for (final AbstractNodeTemplate nodeTemplate : topology.getNodeTemplates()) {
+            if (nodeTemplateHasTests(nodeTemplate)) {
+                final ANodeTemplateActivity activity =
+                    new ANodeTemplateActivity(nodeTemplate.getId() + "__test_activity", "TEST", nodeTemplate);
+                activities.add(activity);
+                node2activityMap.put(nodeTemplate, activity);
+            }
+        }
+
+        final AbstractPlan abstractTestPlan =
+            new AbstractPlan(id, AbstractPlan.PlanType.TEST, definitions, serviceTemplate, activities, links) {};
+        return abstractTestPlan;
+    }
+
+
+    /**
+     * Checks if the policy defines a test
+     *
+     * @param nodeTemplate
+     * @return
+     */
+    private boolean nodeTemplateHasTests(final AbstractNodeTemplate nodeTemplate) {
+        final List<AbstractPolicy> policies = nodeTemplate.getPolicies();
+        for (final AbstractPolicy policy : policies) {
+            // TODO:HUGE work around since there is no way to get the namespaces of a policy yet.
+            final Node policyPropertyDOM = policy.getProperties().getDOMElement().getFirstChild();
+            final String namespace = policyPropertyDOM.getNamespaceURI();
+            final String foundPrefixForTestNS = policyPropertyDOM.lookupPrefix(OPEN_TOSCA_TEST_NAMESPACE);
+            if (foundPrefixForTestNS != null || namespace.equals(OPEN_TOSCA_TEST_NAMESPACE)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
